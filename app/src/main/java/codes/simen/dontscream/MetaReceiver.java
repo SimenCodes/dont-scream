94b97796df70ae99c9d6764ed9f825d497a51dc0
Simen Burud <sb@simen.codes>
2015-04-28 20:01:14 +0200

    Initial commit

---
+++ app/src/main/java/codes/simen/dontscream/MetaReceiver.java
@@ -0,0 +1,90 @@
+package codes.simen.dontscream;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+public class MetaReceiver extends BroadcastReceiver {
+    private static final String logTag = "MetaReceiver";
+
+    public MetaReceiver() {
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        String action = intent.getAction();
+        if (action == null) return;
+        Log.d(logTag, action);
+
+        try {
+            // Debug code for finding fields when adding app support
+            /**final Bundle extras = intent.getExtras();
+            for (String str : extras.keySet()) {
+                final Object o = extras.get(str);
+                Log.v(logTag + " " + str, o != null ? o.toString() : "null");
+            }*/
+
+            boolean playing = intent.getBooleanExtra("playing", true);
+
+            if (!playing) {
+                if (SilentService.isActive) {
+                    context.startService(
+                            new Intent(context, SilentService.class)
+                                    .setAction(SilentService.ACTION_NORMAL_VOLUME)
+                    );
+                }
+                return;
+            }
+
+            long id = -1;
+            if (action.equals("com.spotify.music.metadatachanged")) {
+
+                // In Spotify, the ID is a String
+                String idStr = intent.getStringExtra("id");
+
+                boolean isAd;
+
+                if (idStr != null) {
+                    // The ads have ids containing the word ad
+                    isAd = idStr.startsWith("spotify:ad:");
+                    Log.d(logTag, isAd ? "ad id prefix found!" : "no ad prefix");
+                    // In case they change their naming scheme, we'll revert to using the track length
+                    if (!isAd && !idStr.startsWith("spotify:track:")) {
+                        Log.d(logTag, "no track prefix found either");
+                        isAd = isAdAlternative(intent);
+                    }
+                } else {
+                    // No ID? Let's try the length
+                    isAd = isAdAlternative(intent);
+                }
+
+
+                if (isAd) {
+                    context.startService(
+                            new Intent(context, SilentService.class)
+                                    .setAction(SilentService.ACTION_REDUCE_VOLUME)
+                    );
+                } else if (SilentService.isActive) {
+                    context.startService(
+                            new Intent(context, SilentService.class)
+                                    .setAction(SilentService.ACTION_NORMAL_VOLUME)
+                    );
+                }
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private boolean isAdAlternative(Intent intent) {
+        int length = intent.getIntExtra("length", -1);
+        if (length < 61) {
+            Log.d(logTag, "Short track! Probably an ad");
+            return true;
+        }
+        Log.d(logTag, "Long track, probably not an ad");
+        return false;
+    }
+}
---
+++ app/src/main/java/codes/simen/dontscream/SilentService.java
@@ -0,0 +1,81 @@
+package codes.simen.dontscream;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.media.AudioManager;
+import android.os.IBinder;
+import android.util.Log;
+
+public class SilentService extends Service {
+    private static final String logTag = "SilentService";
+    private static final int NOTIFICATION_ID = 0;
+
+    public SilentService() {
+    }
+
+    public static final String ACTION_REDUCE_VOLUME = "codes.simen.dontscream.ACTION_REDUCE_VOLUME";
+    public static final String ACTION_NORMAL_VOLUME = "codes.simen.dontscream.ACTION_NORMAL_VOLUME";
+
+    public static boolean isActive = false;
+    AudioManager audioManager;
+    NotificationManager notificationManager;
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        audioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
+        notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
+        switch (intent.getAction()) {
+            case ACTION_REDUCE_VOLUME:
+                isActive = true;
+                Log.d(logTag, "Requesting focus");
+                audioManager.requestAudioFocus(audioFocusChangeListener,
+                        AudioManager.STREAM_MUSIC,
+                        AudioManager.AUDIOFOCUS_GAIN_TRANSIENT_MAY_DUCK);
+                notificationManager.notify(NOTIFICATION_ID, getNotification(getApplicationContext()));
+                break;
+            case ACTION_NORMAL_VOLUME:
+                isActive = false;
+                Log.d(logTag, "Abandoning focus");
+                audioManager.abandonAudioFocus(audioFocusChangeListener);
+                notificationManager.cancel(NOTIFICATION_ID);
+                stopSelf();
+                break;
+        }
+
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    private Notification getNotification(Context context) {
+        return new Notification.Builder(context)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle(getString(R.string.notif_title))
+                .setContentText(getString(R.string.notif_text))
+                .setContentIntent(getPendingServiceIntent(ACTION_NORMAL_VOLUME))
+                .setPriority(Notification.PRIORITY_MIN)
+                .build();
+    }
+
+    private PendingIntent getPendingServiceIntent(String action) {
+        return PendingIntent.getService(getApplicationContext(), 0,
+                new Intent(getApplicationContext(), SilentService.class).setAction(action),
+                PendingIntent.FLAG_UPDATE_CURRENT);
+    }
+
+    private AudioManager.OnAudioFocusChangeListener audioFocusChangeListener = new AudioManager.OnAudioFocusChangeListener() {
+        @Override
+        public void onAudioFocusChange(int focusChange) {
+            Log.d(logTag, "Focus changed: " + focusChange);
+        }
+    };
+
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        throw new UnsupportedOperationException("Not possible");
+    }
+}
